ebfcf2b5d60e779b7f134c1a21bf69450e33c950
Ericles-Miller <ericlesmiller15@gmail.com>
2023-01-05 13:48:25 -0300

    finishid Home Page

--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -13,6 +13,10 @@ export const HomeContainer = styled.main`
     flex-direction: column;
     align-items: center;
     gap: 3.5rem;
+
+    button:submit {
+
+    }
   }
 `;
 
@@ -52,3 +56,24 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
+export const StartCountDownButton = styled.button `
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-wight: bold;
+  cursor: pointer;
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme["gray-100"]};
+
+
+  &:hover{
+    background: ${(props) => props.theme["green-700"]};
+  }
+`
