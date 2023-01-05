e5161d091eb287db1b586a9308ee4d260c494071
Ericles-Miller <ericlesmiller15@gmail.com>
2023-01-05 09:54:48 -0300

    delete eslint config and add react router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="danger"></Button>
-      <Button variant="success"></Button>
-
+      <BrowserRouter>
+        <Router></Router>
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
