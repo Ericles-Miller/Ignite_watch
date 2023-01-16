6949758fd6953b14a7ab0826b89f1b00746fac75
Ericles-Miller <ericlesmiller15@gmail.com>
2023-01-16 09:51:35 -0300

    insert componet History in aplication

---
+++ src/components/History/index.tsx
@@ -0,0 +1,60 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Historico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha cerca de 2 horas</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha cerca de 2 horas</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha cerca de 2 horas</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha cerca de 2 horas</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha cerca de 2 horas</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha cerca de 2 horas</td>
+              <td>Em andamento</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
